8ef417c62d7167fe127a303d99538496828ea23d
Jose Ferreira <jpedroferreira95@gmail.com>
2026-02-27 14:30:33 +0000

    feat: add react three fiber & tailwind package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Canvas } from "@react-three/fiber";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+      <div id="canvas-container" className="size-100">
+        <Canvas>
+          <mesh>
+            <boxGeometry args={[2, 3, 4]} />
+            <meshBasicMaterial color="yellow" />
+          </mesh>
+        </Canvas>
+      </div>
+    </>
+  );
 }
 
 export default App;
